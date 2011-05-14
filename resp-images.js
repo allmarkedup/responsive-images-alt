3e94bc74f00c6a56ba773880424c4739ae246332
Mark Perkins <mark@allmarkedup.com>
2011-05-14 12:52:38 +0100

    Fix breakpoint typo.

--- resp-images.js
+++ resp-images.js
@@ -3,7 +3,7 @@
     var doc = win.document,
         date = new Date(),
         winWidth = win.screen.availWidth,
-        breakPoint = ( 'resp_breakPoint' in win ) ? win.rwd_breakPoint : 480;
+        breakPoint = ( 'resp_breakPoint' in win ) ? win.resp_breakPoint : 480;
         
     date.setTime(date.getTime()+(1/*1 day*/*24*60*60*1000));
     doc.cookie = "rwd-resolution=" + winWidth + "; expires=" + date.toGMTString() +"; path=/";
